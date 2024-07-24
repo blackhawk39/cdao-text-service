f6da6d1f04486649c7359f31c9d51a41e2a533a7
blackhawk39 <sourabh7146@gmail.com>
2024-07-24 15:02:18 +0530

    basic text messaging

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-# Sample Hardhat Project
+npm install
+npx  hardhat compile
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+<!-- deploy -->
+npx hardhat run scripts/deploy-and-call.js
 
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat ignition deploy ./ignition/modules/Lock.js
-```
+<!-- corescan -->
---
+++ scripts/deploy-and-call.js
@@ -0,0 +1,29 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const MessagingService = await hre.ethers.getContractFactory("MessagingService");
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  const messagingService = await MessagingService.deploy();
+
+  await messagingService.deployed();
+  console.log("MessagingService contract deployed to:", messagingService.address);
+  console.log("send message: Self")
+  const tx = await await messagingService.sendMessage(deployer.address, "this is demo message")
+  await tx.wait()
+  console.log("explore on corescan", tx);
+  console.log("call getMyMessages() :", await messagingService.getMyMessages())
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
